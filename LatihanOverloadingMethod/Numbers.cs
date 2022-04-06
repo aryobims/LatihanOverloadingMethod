f3418ddebe0a810c4cabf51a8b5efcc0870b8398
aryobims <aryobimo@students.amikom.ac.id>
2022-04-06 16:05:27 +0700

    upload tugas overloading method ke github

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            if (number1 < number2)
+                return number1;
+            else return number2;
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return Math.Min(number1, number2);
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            if (number1 > number2)
+                return number1;
+            else return number2;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return Math.Min(number1, number2);
+        }
+    }
+}
